c936c4535840a25cb26389744fd2fb6f53ad9b4d
vicfd <vicfd@hotmail.com>
2024-07-08 21:20:12 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -123,6 +123,14 @@ resource "aws_instance" "blog" {
               echo "vicfd ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
               EOF
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y s3fs",
+      "sudo mkdir /mnt/s3",
+      "sudo s3fs my-bucket /mnt/s3 -o passwd_file=~/.passwd-s3fs -o allow_other -o umask=022"
+    ]
+
   iam_instance_profile = aws_iam_instance_profile.my_instance_profile.name
 
   tags = {
