c9216694b120bb77a133fb87f05f2e9f2042b3ba
vicfd <vicfd@hotmail.com>
2024-07-08 19:37:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,22 +18,21 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
 resource "aws_security_group" "blog" {
   name = "blog"
   tags = {
     Terraform = "true"
   }
   vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_ssh_in" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -45,7 +44,6 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
@@ -55,7 +53,6 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
   from_port   = 0
@@ -63,4 +60,61 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
+}
+
+# Crear una política de IAM que permita acceso al bucket S3
+data "aws_iam_policy_document" "s3_access_policy" {
+  statement {
+    actions   = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::1234testvcitor",
+      "arn:aws:s3:::1234testvcitor/*"
+    ]
+  }
+}
+
+# Crear un rol de IAM
+resource "aws_iam_role" "my_instance_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Adjuntar la política al rol
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "S3AccessPolicy"
+  description = "Policy to allow access to S3 bucket"
+  policy      = data.aws_iam_policy_document.s3_access_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.my_instance_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+# Crear un perfil de instancia de IAM
+resource "aws_iam_instance_profile" "my_instance_profile" {
+  name = "MyInstanceProfile"
+  role = aws_iam_role.my_instance_role.name
+}
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
+  iam_instance_profile = aws_iam_instance_profile.my_instance_profile.name
+
+  tags = {
+    Name = "Learning Terraform"
+  }
 }
